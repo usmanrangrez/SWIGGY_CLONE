0108df9071546b09bd574d3e8af1d3dfb7079931
usmanrangrez <usmaanrangrez@gmail.com>
2024-01-23 01:33:18 +0530

    Updated UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -9,6 +9,8 @@ const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
   const [copyOfListOfRestaurants, setCopyOfListOfRestaurants] = useState([]);
   const [isTopRatedRestaurants, setIsTopRatedRestaurants] = useState(false);
+  const [searchText, setSearchText] = useState("");
+  const [errorMessage, setErrorMessage] = useState(false);
 
   useEffect(() => {
     fetchData();
@@ -18,8 +20,9 @@ const Body = () => {
     try {
       const data = await axios.get(SWIGGY_API_URL);
       const res =
-        data?.data?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+        data?.data?.data?.cards[3]?.card?.card?.gridElements?.infoWithStyle
           ?.restaurants;
+      console.log(res);
       res
         ? (setListOfRestaurants(res), setCopyOfListOfRestaurants(res))
         : console.log("No valid data recieved from API!");
@@ -36,22 +39,49 @@ const Body = () => {
       } else {
         // If previously it was false, filter the list
         const filteredList = copyOfListOfRestaurants.filter(
-          (res) => res?.info?.avgRating > 4.3
+          (res) => res?.info?.avgRating > 4.0
         );
+        if (filteredList?.length === 0) {
+          setErrorMessage(true);
+        }
         setListOfRestaurants(filteredList);
       }
       return !prevState; // Toggle the state
     });
   };
 
-  const filterResCards = () => {};
+  const filterResCards = () => {
+    if (searchText.length === 0) {
+      setListOfRestaurants(copyOfListOfRestaurants);
+      setErrorMessage(false);
+      return;
+    }
+    const searchBasedFilter = copyOfListOfRestaurants.filter((res) => {
+      return res?.info?.name?.toLowerCase().includes(searchText.toLowerCase());
+    });
+    if (searchBasedFilter?.length === 0) {
+      setErrorMessage(true);
+    }
+    setListOfRestaurants(searchBasedFilter);
+  };
+
+  {
+    if (errorMessage) {
+      return <h4>No Such Restaurants found,Please reload the page!</h4>;
+    }
+  }
 
   return listOfRestaurants?.length === 0 ? (
     <Shimmer />
   ) : (
     <div className="body">
       <div className="filter">
-        <input className="search_res" placeholder="Search for a restaurant!" />
+        <input
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+          className="search_res"
+          placeholder="Search for a restaurant!"
+        />
         <button onClick={filterResCards} className="search_btn">
           Search
         </button>
