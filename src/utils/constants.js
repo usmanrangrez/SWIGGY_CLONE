c1e25b5956b4fd950bed22734b34cb01a7986a1b
usmanrangrez <usmaanrangrez@gmail.com>
2024-02-07 19:07:28 +0530

    solved CORS ERROR

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const IMG_CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/";
 
 export const SWIGGY_API_URL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.8258884&lng=80.0395205&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.8258884&lng=80.0395205&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 export const LOGO_URL =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYDIqZPvNyJdwE7FDgLpTBdNG-jPZ-CBCZmQ&usqp=CAU";
@@ -16,7 +16,7 @@ export const ERROR_IMAGE =
   "https://foodfire-app.netlify.app/404%20Error.59df4a5e.jpg";
 
 export const SWIGGY_RESTAURANT_MENU_API_URL =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.8258884&lng=80.0395205&restaurantId=";
+  "https://cors-anywhere.herokuapp.com/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.8258884&lng=80.0395205&restaurantId=";
 
 export const ITEM_IMG_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/";
